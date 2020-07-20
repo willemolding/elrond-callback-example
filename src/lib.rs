71825fdbf04c761380df29bc87cd57f06ce75eae
Willem Olding <willemolding@gmail.com>
2020-07-20 13:25:31 +1000

    weird amount param name example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #![no_std]
 #![no_main]
 #![allow(non_snake_case)]
@@ -7,21 +6,33 @@
 
 imports!();
 
-#[elrond_wasm_derive::callable(TransferFromProxy)]
-pub trait TransferFrom {
+#[elrond_wasm_derive::callable(TestProxyImpl)]
+pub trait TestProxy {
     #[callback(transferFromCallback)]
-    fn transferFrom(&self, sender: &Address, recipient: &Address, amount: BigUint);
+    fn transferFrom(
+        &self,
+        amountt: BigUint // if this is `amount`it breaks, anything else seems to work
+    );
 }
 
 #[elrond_wasm_derive::contract(ContractImpl)]
-pub trait Contract {
-
+pub trait Contract { 
     #[init]
-    fn init(&self) {
+    fn init(&self) {}
+
+    #[endpoint]
+    fn trigger_remote_call(&self, calee_address: Address, value: BigUint) {
+        let target_contract = contract_proxy!(self, &calee_address, TestProxy);
+        target_contract.transferFrom(
+            value
+        );
     }
 
-    #[callback]
-    fn transferFromCallback(&self, _call_result: AsyncCallResult<()>) {
+    #[endpoint]
+    fn transferFrom(&self, amount: &BigUint) {
+        self.remote_call_received(amount);
     }
 
+    #[event("0x0000000000000000000000000000000000000000000000000000000000000001")]
+    fn remote_call_received(&self, value: &BigUint);
 }
